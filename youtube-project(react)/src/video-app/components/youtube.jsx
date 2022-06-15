2ec45fbf42d91eac0033aa2651937d42fa360cf3
Front-end Web Azərbaycan <frontendweb75@gmail.com>
2022-06-15 17:44:49 +0400

    added

---
+++ youtube-project(react)/src/video-app/components/youtube.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const KEY = "write your google api key in here";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResults: 10,
+    key: KEY,
+  },
+});
